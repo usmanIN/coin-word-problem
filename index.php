bc596fe4751112645ed3a60917905d90f135de24
usmanIN <61617956+usmanIN@users.noreply.github.com>
2020-09-11 13:14:03 +0530

    Add basic template of project

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-echo "Hello World";
-?>
+<html>
+    <head>
+        <title>Demo Forum</title>       
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid text-center">
+            <h1 class="display-2"> Forum</h1>
+            <button class="btn btn-success" >Get Started</button>
+        </div>
+        <div class="container">
+            <ul class="list-group">
+                <li class="list-group-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+                <li class="list-group-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+                <li class="list-group-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+                <li class="list-group-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+                <li class="list-group-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+            </ul>
+        </div>
+    </body>
+</html>
